5e6394eb7001ed176f58c59c6590df316b84f810
Amrut Sagar <amrutsagar06@gmail.com>
2022-11-03 14:46:39 +0530

    server auth email username pwd

---
+++ auth_three/server/index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import {} from "dotenv/config";
+// const express = require("express")     = above exported express can be also imported in this way also.
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+mongoose.connect(
+  process.env.MONGO,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("DB Connected");
+  }
+);
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
+
+//Routes
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      if (password === user.password) {
+        res.send({ message: "Login Successfull", user: user });
+      } else {
+        res.send({ message: "Password didnt match" });
+      }
+    } else {
+      res.send({ message: "User not Registered" });
+    }
+  });
+});
+
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  //hamara user pehle se hi database me created hai , then:
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      res.send({ message: "User already registered" });
+    }
+  });
+  //user ka object creating
+  const user = new User({
+    name,
+    email,
+    password,
+  });
+  user.save((err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send({ message: "Successfully Registered, Please Login now." });
+    }
+  });
+});
+
+app.listen(9002, () => {
+  console.log("BE started at port 9002");
+});
